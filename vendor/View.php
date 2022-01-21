8ffcc3cbb55fccc597f793adae72d8d806cc16cc
Artem Stasenko <85448490+edorofeev89@users.noreply.github.com>
2022-01-21 17:44:56 +0200

    view class

---
+++ vendor/View.php
@@ -0,0 +1,18 @@
+<?php
+
+class view
+{
+
+    public $layout;
+    public $page;
+
+    public function __construct($page = 'index',$layout = 'main')
+    {
+        $this->layout = $layout;
+        $this->page = $page;
+    }
+    public function render()
+    {
+        include_once 'vendor'.DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR. $this->layout.'.php';
+    }
+}
